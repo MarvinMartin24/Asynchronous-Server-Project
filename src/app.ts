f408c404256ec16bfb2c80d8cc3b698516086f98
Martin DESCOTTES <47528109+martindesc@users.noreply.github.com>
2019-12-02 11:47:04 +0100

    connect to db & print doc
    connect to db & print doc

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,22 @@ const PORT = process.env.PORT || 3000;
 
 var express = require("express");
 var app = express();
+var MongoClient = require('mongodb').MongoClient;
+
 app.set('view engine','ejs');
 app.set('views', __dirname + "/../view")
 
+MongoClient.connect("mongodb://localhost:27017/Projet", function(error, db) {
+    //Connection to the database
+    if (error) console.log("error");
+    console.log("Connecté à la base de données 'Projet'");
+
+    //Printing all the docs of the collection in the console
+    var cursor = db.collection('users').find();
+    cursor.each(function(err, doc) {
+        console.log(doc);
+    });
+ });
 
 app.get('/', (req: any, res: any) => {
   res.render('hello.ejs')
