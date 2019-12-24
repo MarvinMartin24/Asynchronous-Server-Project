928e4acfa1bf2812af2452adc4940f7bec509fa2
MarvinMartin24 <mmartin.mmarvin@gmail.com>
2019-12-24 10:40:06 +0100

    front modify user

---
+++ src/view/modify/modify.js
@@ -0,0 +1,50 @@
+let token;
+
+const getToken = () => {
+    token = localStorage.getItem('token')
+    $.ajaxSetup({
+        headers: {
+          'token': token
+        }
+      });
+};
+
+$("form").on("submit", (e) => {
+    e.preventDefault();
+
+    const [firstName, lastName, email, pwd, pwd2] = $("form").serializeArray();
+    //Regex for Valid Characters i.e. Alphabets, Numbers and Space.
+    var regex = /^[A-Za-z0-9 ]+$/
+    var isValid = regex.test(pwd.value);
+
+    if(pwd.value !== pwd2.value){
+        alert("Passwords are different");
+    }
+    else if(isValid){
+        alert("Does not contain Special Characters.");
+    }
+    else if(pwd.value.length < 6){
+        alert("Minimun password lenght is 6");
+    }
+    else{
+        $.ajax({
+            url: "/api/user/update",
+            type: 'PUT',
+            data: {
+                firstName: firstName.value,
+                lastName: lastName.value,
+                email: email.value,
+                password: pwd.value
+            },
+            success: function(result) {
+                if(result.status === "success"){
+                    alert(result.message);
+                    window.location.href = '/me';
+                }
+                if(result.status === "error"){
+                    alert(result.message);
+                }
+            }
+        });
+    }
+});
